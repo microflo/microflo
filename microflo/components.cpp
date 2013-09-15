4308cd25b83d53503b8f11f74ede3bcc888cb72f
Jon Nordby <jononor@gmail.com>
2013-09-15 17:49:50 +0200

    Add BreakBeforeMake component
    First component which performs some level of syncronization in
    collaboration with other components.

--- microflo/components.cpp
+++ microflo/components.cpp
@@ -63,11 +63,13 @@ public:
         // Note: have to match components.json
         const int inPort = 0;
         const int pinConfigPort = 1;
+        const int outPort = 0;
         if (in.isSetup()) {
             outPin = 13; // default
             pinMode(outPin, OUTPUT);
         } else if (port == inPort && in.isBool()) {
             digitalWrite(outPin, in.asBool());
+            send(in, outPort);
         } else if (port == pinConfigPort && in.isNumber()) {
             outPin = in.asInteger();
             pinMode(outPin, OUTPUT);
@@ -343,6 +345,76 @@ public:
             send(Packet(MsgBracketEnd));
         }
     }
+};
+
+// IDEA: ability to express components as finite state machines using a DSL and/or GUI
+class BreakBeforeMake : public Component
+{
+public:
+    BreakBeforeMake() : state(Init) {}
+    virtual void process(Packet in, int port) {
+        const int inPort = 0;
+        const int out1MonitorPort = 1;
+        const int out2MonitorPort = 2;
+
+        const int out1Port = 0;
+        const int out2Port = 1;
+
+        // XXX: inputs are ignored while in transition
+        switch (state) {
+        case Init:
+            state = SettledOff;
+            break;
+        case WaitFor2Off:
+            if (port == out2MonitorPort && !in.asBool()) {
+                send(Packet((bool)true), out1Port);
+                state = WaitFor1On;
+            }
+            break;
+        case WaitFor1On:
+            if (port == out1MonitorPort && in.asBool()) {
+                state = SettledOff;
+            }
+            break;
+        case SettledOff:
+            if (port == inPort && in.asBool()) {
+                send(Packet((bool)false), out1Port);
+                state = WaitFor1Off;
+            }
+            break;
+        case WaitFor1Off:
+            if (port == out1MonitorPort && !in.asBool()) {
+                send(Packet((bool)true), out2Port);
+                state = WaitFor2On;
+            }
+            break;
+        case WaitFor2On:
+            if (port == out2MonitorPort && in.asBool()) {
+                state = SettledOn;
+            }
+            break;
+        case SettledOn:
+            if (port == inPort && !in.asBool()) {
+                send(Packet((bool)false), out2Port);
+                state = WaitFor2Off;
+            }
+        default:
+            break;
+        }
+    }
+private:
+    enum State {
+        Init,
+        WaitFor1On,
+        WaitFor1Off,
+        WaitFor2On,
+        WaitFor2Off,
+        SettledOn,
+        SettledOff
+    };
+
+private:
+    enum State state;
 };
 
 class Delimit : public Component {
