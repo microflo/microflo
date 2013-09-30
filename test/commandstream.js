50614dd3d9014facd438b1e58e9dec72a1cbdc74
Jon Nordby <jononor@gmail.com>
2013-09-30 22:53:57 +0200

    Add a simple test for commandstream generation

---
+++ test/commandstream.js
@@ -0,0 +1,23 @@
+/* MicroFlo - Flow-Based Programming for microcontrollers
+ * Copyright (c) 2013 Jon Nordby <jononor@gmail.com>
+ * MicroFlo may be freely distributed under the MIT license
+ */
+
+var microflo = require("../microflo");
+var addon = require("../build/Release/MicroFlo.node");
+var assert = require("assert");
+var fbp = require("fbp");
+
+describe('Commandstream generation', function(){
+  describe('from a simple input FBP', function(){
+      var input = "in(SerialIn) OUT -> IN f(Forward) OUT -> IN out(SerialOut)";
+      var expect = Buffer([117,67,47,70,108,111,48,49,
+                           10,0,0,0,0,0,0,0,11,8,0,0,0,0,0,0,
+                           11,3,0,0,0,0,0,0,11,9,0,0,0,0,0,0,
+                           12,0,1,0,0,0,0,0,12,1,2,0,0,0,0,0]);
+      it('parsing should give known valid output', function(){
+          var out = microflo.cmdStreamFromGraph(microflo.componentLib, fbp.parse(input));
+          assert.equal(out.toString("hex"), expect.toString("hex"));
+    })
+  })
+})
