microflo/microflo

commit 4a6a3525b8d0cb23c35ef1e79a3e216be04b43e6

Make DallasTemperature dependency optional, opt-in

Makefile
@@ -16,7 +16,7 @@ build: definitions
 	cd build/arduino/lib && test -e patched || patch -p0 < ../../../thirdparty/OneWire.patch
 	touch build/arduino/lib/patched
 	node microflo.js generate $(GRAPH) build/arduino/src/firmware.ino
-	cd build/arduino && ino build --board-model=$(MODEL)
+	cd build/arduino && ino build --board-model=$(MODEL) --cppflags="-DHAVE_DALLAS_TEMPERATURE"
 	avr-size -A build/arduino/.build/$(MODEL)/firmware.elf
 
 upload: build

microflo/components.cpp
@@ -237,7 +237,7 @@ private:
     unsigned long interval;
 };
 
-#ifdef ARDUINO
+#ifdef HAVE_DALLAS_TEMPERATURE
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
@@ -293,7 +293,7 @@ private:
 };
 #else
 class ReadDallasTemperature : public DummyComponent {};
-#endif // ARDUINO
+#endif
 
 class ToggleBoolean : public Component {
 public:

commit 568f3870704ef1b12f2381a4f7fe1da5ab6427c6

Fix binary size over 32k causing uploads to fail
We were replacing the compilation options instead of just appending our custom defines.
Ino really should have a nicer way to pass in defines.
Regression since 4a6a3525b8d0cb23c35ef1e79a3e216be04b43e6

Makefile
@@ -4,6 +4,8 @@ GRAPH=examples/blink.fbp
 MODEL=uno
 REPORTER=spec
 VERSION=$(shell git describe --tags)
+CPPFLAGS=-ffunction-sections -fdata-sections -g -Os -w
+DEFINES=-DHAVE_DALLAS_TEMPERATURE
 
 all: build
 
@@ -17,7 +19,7 @@ build: definitions
 	cd build/arduino/lib && test -e patched || patch -p0 < ../../../thirdparty/OneWire.patch
 	touch build/arduino/lib/patched
 	node microflo.js generate $(GRAPH) build/arduino/src/firmware.ino
-	cd build/arduino && ino build --board-model=$(MODEL) --cppflags="-DHAVE_DALLAS_TEMPERATURE"
+	cd build/arduino && ino build --board-model=$(MODEL) --cppflags="$(CPPFLAGS) $(DEFINES)"
 	avr-size -A build/arduino/.build/$(MODEL)/firmware.elf
 
 upload: build