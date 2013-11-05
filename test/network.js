microflo/microflo

commit 19bd874469412a3ee4bcb259378bb8d79cfa1798

Mark end of command stream, only start network when reached

microflo/microflo.h
@@ -98,6 +98,8 @@ public:
     Network(IO *io);
 
     void reset();
+    void start();
+
     int addNode(Component *node);
     void connect(Component *src, int srcPort, Component *target, int targetPort);
     void connect(int srcId, int srcPort, int targetId, int targetPort);
@@ -116,6 +118,11 @@ public:
 private:
     void deliverMessages(int firstIndex, int lastIndex);
     void processMessages();
+    enum State {
+        Invalid = -1,
+        Stopped,
+        Running
+    };
 
 private:
     Component *nodes[MAX_NODES];
@@ -128,6 +135,7 @@ private:
     AddNodeNotification addNodeNotify;
     NodeConnectNotification nodeConnectNotify;
     IO *io;
+    State state;
 };
 
 struct Connection {

microflo.js
@@ -216,6 +216,9 @@ var cmdStreamFromGraph = function(componentLib, graph) {
         }
     });
 
+    // Mark end of commands
+    index += writeCmd(buffer, index, cmdFormat.commands.End.id);
+
     // Attach the mapping so others can use it later
     graph.nodeMap = nodeMap;
 

microflo/microflo.cpp
@@ -117,8 +117,14 @@ void GraphStreamer::parseByte(char b) {
             if (cmd >= GraphCmdInvalid) {
                 state = Invalid; // XXX: or maybe just ignore?
             } else {
-                if (cmd == GraphCmdReset) {
-                    // TODO: implement
+                if (cmd == GraphCmdEnd) {
+                    network->start();
+                    state = ParseHeader;
+#ifdef DEBUG
+                    Serial.println("Starting network");
+#endif
+                } else if (cmd == GraphCmdReset) {
+                    network->reset();
                 } else if (cmd == GraphCmdCreateComponent) {
                     ComponentId id = (ComponentId)buffer[1];
                     // FIXME: validate
@@ -199,11 +205,14 @@ Network::Network(IO *io)
     : lastAddedNodeIndex(0)
     , messageWriteIndex(0)
     , messageReadIndex(0)
+
     , messageSentNotify(0)
     , messageDeliveredNotify(0)
     , addNodeNotify(0)
     , nodeConnectNotify(0)
+
     , io(io)
+    , state(Stopped)
 {
     for (int i=0; i<MAX_NODES; i++) {
         nodes[i] = 0;
@@ -273,6 +282,10 @@ void Network::sendMessage(int targetId, int targetPort, const Packet &pkg) {
 }
 
 void Network::runSetup() {
+    if (state != Running) {
+        return;
+    }
+
     for (int i=0; i<MAX_NODES; i++) {
         if (nodes[i]) {
             nodes[i]->process(Packet(MsgSetup), -1);
@@ -281,6 +294,9 @@ void Network::runSetup() {
 }
 
 void Network::runTick() {
+    if (state != Running) {
+        return;
+    }
 
     // TODO: consider the balance between scheduling and messaging (bounded-buffer problem)
 
@@ -324,5 +340,20 @@ int Network::addNode(Component *node) {
 }
 
 void Network::reset() {
-    // FIXME: implement
+    state = Stopped;
+    for (int i=0; i<MAX_NODES; i++) {
+        if (nodes[i]) {
+            delete (nodes[i]);
+            nodes[i] = 0;
+        }
+    }
+    lastAddedNodeIndex = 0;
+    messageWriteIndex = 0;
+    messageReadIndex = 0;
+}
+
+void Network::start() {
+    state = Running;
+
+    runSetup();
 }

test/commandstream.js
@@ -12,9 +12,10 @@ describe('Commandstream generation', function(){
   describe('from a simple input FBP', function(){
       var input = "in(SerialIn) OUT -> IN f(Forward) OUT -> IN out(SerialOut)";
       var expect = Buffer([117,67,47,70,108,111,48,49,
-                           10,0,0,0,0,0,0,0,11,8,0,0,0,0,0,0,
-                           11,3,0,0,0,0,0,0,11,9,0,0,0,0,0,0,
-                           12,0,1,0,0,0,0,0,12,1,2,0,0,0,0,0]);
+                           10,0,0,0,0,0,0,0, 11,8,0,0,0,0,0,0,
+                           11,3,0,0,0,0,0,0, 11,9,0,0,0,0,0,0,
+                           12,0,1,0,0,0,0,0, 12,1,2,0,0,0,0,0,
+                           14,0,0,0,0,0,0,0 ]);
       it('parsing should give known valid output', function(){
           var out = microflo.cmdStreamFromGraph(microflo.componentLib, fbp.parse(input));
           assert.equal(out.toString("hex"), expect.toString("hex"));

commit 5acd329f79ebfccf0390c471b7073bad71f0b7cc

Fix stalling test due to API change
Broken since 19bd874469412a3ee4bcb259378bb8d79cfa1798
Whatever manages the Network now need to call start() instead of runSetup()
Also add a timeout to the test, and make Network::runSetup() private.

microflo.cc
@@ -121,7 +121,7 @@ private:
     static v8::Handle<v8::Value> AddNode(const v8::Arguments& args);
     static v8::Handle<v8::Value> Connect(const v8::Arguments& args);
     static v8::Handle<v8::Value> SendMessage(const v8::Arguments& args);
-    static v8::Handle<v8::Value> RunSetup(const v8::Arguments& args);
+    static v8::Handle<v8::Value> Start(const v8::Arguments& args);
     static v8::Handle<v8::Value> RunTick(const v8::Arguments& args);
 private:
     ;
@@ -147,8 +147,8 @@ void JavaScriptNetwork::Init(v8::Handle<v8::Object> exports) {
                                 v8::FunctionTemplate::New(Connect)->GetFunction());
   tpl->PrototypeTemplate()->Set(v8::String::NewSymbol("sendMessage"),
                                 v8::FunctionTemplate::New(SendMessage)->GetFunction());
-  tpl->PrototypeTemplate()->Set(v8::String::NewSymbol("runSetup"),
-                                v8::FunctionTemplate::New(RunSetup)->GetFunction());
+  tpl->PrototypeTemplate()->Set(v8::String::NewSymbol("start"),
+                                v8::FunctionTemplate::New(Start)->GetFunction());
   tpl->PrototypeTemplate()->Set(v8::String::NewSymbol("runTick"),
                                 v8::FunctionTemplate::New(RunTick)->GetFunction());
 
@@ -169,10 +169,10 @@ v8::Handle<v8::Value> JavaScriptNetwork::RunTick(const v8::Arguments& args) {
   obj->runTick();
   return scope.Close(v8::Undefined());
 }
-v8::Handle<v8::Value> JavaScriptNetwork::RunSetup(const v8::Arguments& args) {
+v8::Handle<v8::Value> JavaScriptNetwork::Start(const v8::Arguments& args) {
   v8::HandleScope scope;
   JavaScriptNetwork* obj = node::ObjectWrap::Unwrap<JavaScriptNetwork>(args.This());
-  obj->runSetup();
+  obj->start();
   return scope.Close(v8::Undefined());
 }
 

microflo/microflo.h
@@ -113,9 +113,9 @@ public:
                           NodeConnectNotification nodeConnect,
                           AddNodeNotification addNode);
 
-    void runSetup();
     void runTick();
 private:
+    void runSetup();
     void deliverMessages(int firstIndex, int lastIndex);
     void processMessages();
     enum State {

test/network.js
@@ -46,9 +46,15 @@ describe('Network', function(){
             net.sendMessage(0, 0, messages[i]);
         }
 
-        net.runSetup();
+        var deadline = new Date().getTime() + 1*1000; // ms
+        net.start();
         while (compare.expectingMore()) {
             net.runTick();
+            if (new Date().getTime() > deadline) {
+                assert.fail(compare.actual.length, compare.expected.length,
+                            "Did not get expected packages within deadline");
+                break;
+            }
         }
         assert.equal(compare.actual.length, 10);
         assert.deepEqual(compare.actual, compare.expected);