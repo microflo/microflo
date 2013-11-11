5d851f1412b1035ebde95faaeb18d1d66e04ed57
Jon Nordby <jononor@gmail.com>
2013-11-11 21:56:03 +0100

    components: Add Route

--- microflo/components.cpp
+++ microflo/components.cpp
@@ -539,4 +539,37 @@ private:
     bool enabled;
 };
 
+class Route : public Component {
+
+public:
+    Route() : activePort(0) {
+        for (int i=0; i<RoutePorts::InPorts::in9; i++) {
+            lastPackets[i] = Packet(MsgInvalid);
+        }
+    }
+
+    virtual void process(Packet in, int port) {
+        using namespace RoutePorts;
+        if (port == InPorts::port) {
+            activePort = in.asInteger();
+            if (activePort > 0 && activePort < InPorts::in9) {
+                Packet &p = lastPackets[activePort];
+                if (p.type() != MsgInvalid) {
+                    send(p);
+                }
+            }
+        } else {
+            if (in.isData()) {
+                lastPackets[port] = in;
+                if (port == activePort) {
+                    send(in);
+                }
+            }
+        }
+    }
+private:
+    Packet lastPackets[RoutePorts::InPorts::in9];
+    int activePort;
+};
+
 #include "components-gen-bottom.hpp"
