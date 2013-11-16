8d189bf6b7d605c2e9bce0dcbf8b43f7b98279ef
Jon Nordby <jononor@gmail.com>
2013-11-16 17:05:36 +0100

    components: Add BooleanOr

--- microflo/components.cpp
+++ microflo/components.cpp
@@ -332,6 +332,23 @@ public:
     }
 };
 
+class BooleanOr : public Component {
+public:
+    BooleanOr() {
+        lastState[0] = false;
+        lastState[1] = false;
+    }
+
+    virtual void process(Packet in, int port) {
+        if (in.isData() && port <= 1) {
+            lastState[port] = in.asBool();
+            send((lastState[0] || lastState[1]) ? Packet((bool)true) : Packet((bool)false));
+        }
+    }
+private:
+    bool lastState[2];
+};
+
 class ArduinoUno : public Component {
 public:
     virtual void process(Packet in, int port) {
