133c03e70a0623c3f74e78ea0b0fbb46e87592ad
Jon Nordby <jononor@gmail.com>
2013-12-01 16:08:40 +0100

    test: Add very basic test for WebSocket (NoFlo) API

---
+++ test/websocketapi.js
@@ -0,0 +1,65 @@
+/* MicroFlo - Flow-Based Programming for microcontrollers
+ * Copyright (c) 2013 Jon Nordby <jononor@gmail.com>
+ * MicroFlo may be freely distributed under the MIT license
+ */
+
+var assert = require("assert")
+var microflo = require("../microflo");
+var websocket = require("websocket");
+
+var componentLib = new microflo.ComponentLibrary(require("../microflo/components.json"));
+
+describe('WebSocket API', function(){
+    before(function () {
+        microflo.setupRuntime({"parent": {"port": 3888}});
+    });
+
+    after(function () {
+        // FIXME: allow teardown
+    });
+    describe('sending component list command', function(){
+        it('should return all available components', function(done){
+
+            var client = new websocket.client();
+
+            client.on('connectFailed', function(error) {
+                assert.fail("connect success", "connect failed", error.toString());
+            });
+
+            var expectedComponents = componentLib.listComponents();
+            var receivedComponent = [];
+
+            client.on('connect', function(connection) {
+                connection.on('error', function(error) {
+                    assert.fail("connect OK", "connect error", error.toString());
+                });
+                connection.on('close', function() {
+
+                });
+                connection.on('message', function(message) {
+                    if (message.type === 'utf8') {
+                        var response = JSON.parse(message.utf8Data);
+                        assert.equal(response.protocol, "component");
+                        assert.equal(response.command, "component");
+                        receivedComponent.push(response.payload);
+
+                        if (receivedComponent.length === Object.keys(expectedComponents).length) {
+                            // TODO: verify value of component
+                            done();
+                        }
+                    }
+                });
+
+                function listComponents() {
+                    if (connection.connected) {
+                        var m = {"protocol": "component", "command": "list"};
+                        connection.sendUTF(JSON.stringify(m));
+                    }
+                }
+                listComponents();
+            });
+
+            client.connect('ws://localhost:3888/', "noflo");
+    })
+  })
+})
