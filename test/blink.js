7b947e9b32f67ec56d53022db09eaa9ea5d68e04
Jon Nordby <jononor@gmail.com>
2014-01-26 18:26:07 +0100

    test: Actually verify IO behavior of Blink program

--- test/blink.js
+++ test/blink.js
@@ -11,17 +11,39 @@ var microflo = require("../lib/microflo");
 describe('a Blink program', function(){
     var simulator = new microflo.simulator.RuntimeSimulator();
     var prog = "\
-            timer(Timer) OUT -> IN toggle(ToggleBoolean) \
-            toggle() OUT -> IN led(DigitalWrite) \
+            timer(Timer) OUT -> IN toggle(ToggleBoolean) OUT -> IN led(DigitalWrite)\
             '300' -> INTERVAL timer() \
             '13' -> PIN led()";
     simulator.start();
 
     it('should load', function(finish){
+        simulator.io.state.digitalOutputs[13] = true;
+
         simulator.uploadFBP(prog, function () {
             var nodes = simulator.network.getNodes();
             assert.equal(nodes.length, 3);
             finish();
         });
+    })
+    it('and set initial output LOW', function(){
+        assert.equal(simulator.io.state.digitalOutputs[13], false);
+    })
+    it('should go HIGH when timer expires', function(finish){
+        simulator.io.state.currentTimeMs += 301;
+        console.log("first try");
+        simulator.io.waitForChange(function () {
+            assert.equal(simulator.io.state.digitalOutputs[13], true);
+            finish();
+        });
+    })
+    it('and then toggle LOW when timer expires again', function(finish){
+        simulator.io.state.currentTimeMs += 301;
+        simulator.io.waitForChange(function () {
+            assert.equal(simulator.io.state.digitalOutputs[13], false);
+            finish();
+        });
+    })
+    after(function () {
+        simulator.stop();
     })
 })
