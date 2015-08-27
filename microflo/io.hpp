e08a4fbdb8e3796f47268bcc4a21240c1da94dcf
Jon Nordby <jononor@gmail.com>
2015-08-27 15:56:09 +0200

    engine: Add a NullIO backend
    Useful when doing board bringup

---
+++ microflo/io.hpp
@@ -0,0 +1,66 @@
+class NullIO : public IO {
+
+public:
+    NullIO()
+    {
+    }
+    ~NullIO()
+    {
+    }
+
+    // Serial
+    virtual void SerialBegin(uint8_t serialDevice, int baudrate) {
+        MICROFLO_DEBUG(debug, DebugLevelError, DebugIoOperationNotImplemented);
+    }
+    virtual long SerialDataAvailable(uint8_t serialDevice) {
+        MICROFLO_DEBUG(debug, DebugLevelError, DebugIoOperationNotImplemented);
+        return 0;
+    }
+    virtual unsigned char SerialRead(uint8_t serialDevice) {
+        MICROFLO_DEBUG(debug, DebugLevelError, DebugIoOperationNotImplemented);
+        return '\0';
+    }
+    virtual void SerialWrite(uint8_t serialDevice, unsigned char b) {
+        MICROFLO_DEBUG(debug, DebugLevelError, DebugIoOperationNotImplemented);
+    }
+
+    // Pin config
+    virtual void PinSetMode(MicroFlo::PinId pin, IO::PinMode mode) {
+        MICROFLO_DEBUG(debug, DebugLevelError, DebugIoOperationNotImplemented);
+    }
+    virtual void PinSetPullup(MicroFlo::PinId pin, IO::PullupMode mode) {
+        // TODO: support pullup/pulldown config on common boards like RPi
+        // Not exposed in sysfs, need to prod registers directly.
+        // http://elinux.org/RPi_Low-level_peripherals#GPIO_Pull_Up.2FPull_Down_Register_Example
+        MICROFLO_DEBUG(debug, DebugLevelError, DebugIoOperationNotImplemented);
+    }
+
+    // Digital
+    virtual void DigitalWrite(MicroFlo::PinId pin, bool val) {
+        MICROFLO_DEBUG(debug, DebugLevelError, DebugIoOperationNotImplemented);
+    }
+    virtual bool DigitalRead(MicroFlo::PinId pin) {
+        MICROFLO_DEBUG(debug, DebugLevelError, DebugIoOperationNotImplemented);
+        return false;
+    }
+
+    // Analog
+    virtual long AnalogRead(MicroFlo::PinId pin) {
+        MICROFLO_DEBUG(debug, DebugLevelError, DebugIoOperationNotImplemented);
+        return 0;
+    }
+    virtual void PwmWrite(MicroFlo::PinId pin, long dutyPercent) {
+        MICROFLO_DEBUG(debug, DebugLevelError, DebugIoOperationNotImplemented);
+    }
+
+    // Timer
+    virtual long TimerCurrentMs() {
+        MICROFLO_DEBUG(debug, DebugLevelError, DebugIoOperationNotImplemented);
+        return 0;
+    }
+
+    virtual void AttachExternalInterrupt(uint8_t interrupt, IO::Interrupt::Mode mode,
+                                        IOInterruptFunction func, void *user) {
+        MICROFLO_DEBUG(debug, DebugLevelError, DebugIoOperationNotImplemented);
+    }
+};
