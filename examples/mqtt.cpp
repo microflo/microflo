659b2ec7230512fa305a08c1dca04c9f24337005
Jon Nordby <jononor@gmail.com>
2016-04-17 15:54:48 +0200

    mqtt: Introspect exported ports from graph
    No hardcoding of the setup, information is now
    sourced from the .FBP / .JSON file

--- examples/mqtt.cpp
+++ examples/mqtt.cpp
@@ -1,7 +1,12 @@
 
 #define MICROFLO_EMBED_GRAPH
 
+// FIXME: allow to receive PacketSent node/port without target
+
 /* microflo_graph fbp
+     INPORT=first.IN:INPUT
+     OUTPORT=second.OUT:OUTPUT
+
      first(Forward) OUT -> IN second(Forward) OUT -> IN third(Forward)
 microflo_graph */
 
@@ -15,6 +20,7 @@ microflo_graph */
 //#include "componentlib-source.hpp"
 #include "componentlib.hpp"
 #include "main.h" // generated graph definition
+#include "main_maps.h" // generated graph metadata
 #include "microflo.hpp"
 #include "linux.hpp"
 
@@ -24,17 +30,27 @@ static void die(const char *msg) {
     exit(1);
 }
 
+// TODO: introspect from Network/cmdstream instead of graph metadata
 bool findPorts(ParticipantInfo *info) {
-    // FIXME: actually introspect from Network/graph data, instead of hardcode
-    // can first be done by looking at static graph data,
-    // which requires looking at .inports and .outports of graph
+    // FIXME: don't assume graph data is in 'graph_*'
 
-    info->addInport("input", 1, 0);
-    info->addOutport("output", 2, 0);
+    for (size_t i=0; i<graph_inports_length; i++) {
+        const char *name = graph_inports_name[i];
+        MicroFlo::PortId portId = graph_inports_port[i];
+        MicroFlo::NodeId nodeId = graph_inports_node[i];
+        info->addInport(name, nodeId, portId);
+    }
+    for (size_t i=0; i<graph_outports_length; i++) {
+        const char *name = graph_outports_name[i];
+        MicroFlo::PortId portId = graph_outports_port[i];
+        MicroFlo::NodeId nodeId = graph_outports_node[i];
+        info->addOutport(name, nodeId, portId);
+    }
+
     return true;
 }
 
-// TODO: move all this into linux_mqtt_main, and add a compile target for that
+// TODO: move all this into linux_mqtt_main, and add a build target for that
 int main(int argc, char **argv) {
     LinuxIO io;
     NullHostTransport transport;
