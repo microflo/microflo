8309df6b4302ef12696cd2df08a33c459d6aa329
Jon Nordby <jononor@gmail.com>
2017-05-11 01:58:06 +0200

    subscribeToPort(): Check that node instance is valid

--- microflo/microflo.cpp
+++ microflo/microflo.cpp
@@ -511,6 +511,8 @@ void Network::subscribeToPort(MicroFlo::NodeId nodeId, MicroFlo::PortId portId, 
     MICROFLO_RETURN_IF_FAIL(MICROFLO_VALID_NODEID(nodeId),
                             notificationHandler, DebugLevelError, DebugSubscribePortInvalidNode);
     Component *c = nodes[nodeId];
+    MICROFLO_RETURN_IF_FAIL(c,
+                            notificationHandler, DebugLevelError, DebugSubscribePortInvalidNode);
     MICROFLO_RETURN_IF_FAIL(portId >= 0 && portId < c->nPorts,
                             notificationHandler, DebugLevelError, DebugSubscribePortInvalidPort);
 
